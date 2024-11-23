ad84ad0da484b1313a1f15f89b0f31043b3a3d39
tritoncg <admin@tritoncg.com>
2024-11-23 00:57:00 +0000

    installing three.js and vercel.json"

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,24 @@ import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 import './App.css';
 import AppRouter from './pages/Router';
+import * as THREE from 'three'
 
 function App() {
+  const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <AppRouter/>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <GlobalStyles />
+    //   <AppRouter/>
+    // </ThemeProvider>
+
+    <>
+        
+    </>
   );
 }
 
