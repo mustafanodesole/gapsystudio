908ab30a4e0151bc61370ab3ea5b95d5839dd222
tritoncg <admin@tritoncg.com>
2024-11-23 01:07:11 +0000

    vercel issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,14 @@ import { theme } from './styles/theme';
 import './App.css';
 import AppRouter from './pages/Router';
 import * as THREE from 'three'
+import ThreeScene from './components/ThreeScene';
 
 function App() {
-  const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
   return (
-    // <ThemeProvider theme={theme}>
-    //   <GlobalStyles />
-    //   <AppRouter/>
-    // </ThemeProvider>
-
-    <>
-        
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRouter/>
+    </ThemeProvider>
   );
 }
 
